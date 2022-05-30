ee1528a758dfbcf73d60df1828b22abbdd480882
noodles <729804002@qq.com>
2022-05-30 17:53:30 +0800

    fix: asdf

--- MV/index.tsx
+++ MV/index.tsx
@@ -1,10 +1,9 @@
 export const b = 4
 
-var str = "aaaaaaabbbbbbbb";
+var str = "aaaaaaabbbbs";
 //时间戳转换为日期
 function getLocalTime(nS) {
     return new Date(parseInt(nS) * 1000).toLocaleString().replace(/:\d{1,2}$/, ' ');
 }
 
-var a = str;
- 
+var a = 2355;

81187f70f942b228dd2b9121750b73985b1f2539
noodles <729804002@qq.com>
2022-05-31 09:52:56 +0800

    feat: 同事的分支提交

--- MV/index.tsx
+++ MV/index.tsx
@@ -1,10 +1,10 @@
-export const b = 4
-
-var str = "aaaaaaabbbbbbbb";
+ 
+var str = "aaa23444bbbbbb";
 //时间戳转换为日期
 function getLocalTime(nS) {
     return new Date(parseInt(nS) * 1000).toLocaleString().replace(/:\d{1,2}$/, ' ');
 }
 
 var a = str;
+var ss = a.substring(0, a.length - 1);
  
